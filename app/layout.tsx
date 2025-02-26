334542e4b6f9a9b92f5580a0f273ffba30e6f21a
georgeankumensah <georgeankumensah@gmail.com>
2025-02-26 13:44:34 +0000

    fix: meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,26 +5,10 @@ import './globals.css'
 import { useEffect } from "react"
 import { getUser } from "@/lib/auth"
 import "@/styles/globals.css"
-import { Metadata as NextMetadata } from 'next';
+import Head from "next/head"
 
 
-type CustomMetadata = NextMetadata & {
-  meta: {
-    name: string;
-    content: string;
-  }[];
-}; 
 
-export const metadata: CustomMetadata = {
-  title: "Peso - Contemporary Fashion",
-  description: "Shop the latest contemporary fashion at Peso",
-  meta: [
-    {
-      name: "google-site-verification",
-      content: "Obu7K2aofd4rR9gMAigeCdRreFbZfc9liMYVWxkP5Dc",
-    },
-  ],
-};
 
 export default function RootLayout({
   children,
@@ -38,6 +22,9 @@ export default function RootLayout({
   )
   return (
     <html lang="en">
+      <Head>
+      <meta name="google-site-verification" content="Obu7K2aofd4rR9gMAigeCdRreFbZfc9liMYVWxkP5Dc" />
+      </Head>
       <body>
        
           {children}
